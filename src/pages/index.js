6cf073a5a1abee9819b19304d193da7f1b06a0ba
maxnvdm <maxnvdm@gmail.com>
2020-07-19 13:31:13 +0200

    Final updates before submission

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,17 @@ const IndexPage = () => {
 		const { data = [] } = response;
 		const hasData = Array.isArray(data) && data.length > 0;
 
-		if (!hasData) return;
+		// Trying to find the max and min cases to normalize data
+		var min = data[0].casesPerOneMillion;
+		var max = data[0].casesPerOneMillion;
+		var i;
+		for (i = 0; i < data.length; i++) {
+			if (data[i].casesPerOneMillion < min) min = data[i].casesPerOneMillion;
+			if (data[i].casesPerOneMillion > max) max = data[i].casesPerOneMillion;
+		}
 
+		if (!hasData) return;
+		// console.log(data);
 		const geoJson = {
 			type: 'FeatureCollection',
 			features: data.map((country = {}) => {
@@ -62,7 +71,14 @@ const IndexPage = () => {
 				let updatedFormatted;
 				let casesString;
 
-				const { country, updated, cases, deaths, recovered } = properties;
+				const {
+					country,
+					updated,
+					cases,
+					deaths,
+					recovered,
+					casesPerOneMillion,
+				} = properties;
 
 				casesString = `${cases}`;
 
@@ -74,9 +90,24 @@ const IndexPage = () => {
 					updatedFormatted = new Date(updated).toLocaleString();
 				}
 
+				// let color1 = `rgb(244, 226, 216)`;
+				// let color2 = `rgb(186, 83, 112)`;
+				let color2 = [244, 226, 216];
+				let color1 = [186, 83, 112];
+				var p = (cases - min) / (max - min);
+				var w = p * 2 - 1;
+				var w1 = (w / 1 + 1) / 2;
+				var w2 = 1 - w1;
+				var iconColor = [
+					Math.round(color1[0] * w1 + color2[0] * w2),
+					Math.round(color1[1] * w1 + color2[1] * w2),
+					Math.round(color1[2] * w1 + color2[2] * w2),
+				];
+				iconColor = `rgb(${iconColor[0]},${iconColor[1]},${iconColor[2]})`;
+
 				const html = `
-        <span class="icon-marker">
-          <span class="icon-marker-tooltip">
+        	<span class="icon-marker" style="background-color: ${iconColor};">
+          	<span class="icon-marker-tooltip" style="background-color: ${iconColor};">
             <h2>${country}</h2>
             <ul>
               <li><strong>Confirmed:</strong> ${cases}</li>
@@ -116,16 +147,16 @@ const IndexPage = () => {
 			</Helmet>
 
 			<Map {...mapSettings} c />
+			<div id="slider"></div>
+			<p style={{ textAlign: 'left' }}>
+				{'Less cases'}
+				<span style={{ float: 'right' }}>{'More cases'}</span>
+			</p>
 
 			<Container type="content" className="text-center home-start">
-				<h2>Still Getting Started?</h2>
-				<p>Run the following in your terminal!</p>
-				<Snippet>
-					gatsby new [directory]
-					https://github.com/colbyfayock/gatsby-starter-leaflet
-				</Snippet>
-				<p className="note">
-					Note: Gatsby CLI required globally for the above command
+				<h2>Welcome to another COVID-19 stats website!</h2>
+				<p>This welcome page displays stats from all over the world for different countries.</p>
+				<p>The color of each country icon represents the confirmed cases per million of the population.
 				</p>
 			</Container>
 		</Layout>
